1fe62202c6c38b8d346908d9f47dcf34a0383ed0
sameer666 <singh.sameer9415@gmail.com>
2024-03-18 13:11:31 +0530

    gcp usecase

--- modules/gcp/providers.tf
+++ modules/gcp/providers.tf
@@ -2,7 +2,7 @@
 # Providers
 ###############
 provider "google" {
-  #credentials = file("./gcp-creds.json")
+  credentials = file("./gcp-creds.json")
   project = var.project_id
   region  = var.region
 }
