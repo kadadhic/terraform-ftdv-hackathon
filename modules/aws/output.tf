5996dfe341044cb4b2a8fbf7e191d1ae7b7e99a3
Suraj <snetlamu@cisco.com>
2024-08-26 11:38:56 +0530

    Fixed error

--- modules/aws/output.tf
+++ modules/aws/output.tf
@@ -1,5 +1,5 @@
 output "FMC_URL" {
-  value = "https://${aws_eip.fmcmgmt-EIP.public_ip}"
+  value = "https://${aws_eip.fmcmgmt-EIP[0].public_ip}"
 }
 output "SSH_Command_FTD" {
   value = "ssh -i ${var.prefix}-${var.keyname} admin@${aws_eip.ftd01mgmt-EIP.public_ip}"
